01528deed8ce7dc7d143ad9d6952f42bee74baf0
FIC <FIC@info.local>
2019-09-13 09:36:57 -0300

    Arquivos Inclusos

---
+++ Aula1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aula1 //COMENTARIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 23;
+            int num2 = 5;
+            int num3, num4, num5 = 60;
+            float media;
+
+            bool resultado = false;
+
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
