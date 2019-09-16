6595be90fb03083ebf421c9a6e1576cfce47b700
FIC <FIC@info.local>
2019-09-16 10:07:33 -0300

    modificação dos ex 6 e 7

--- Atividades de Aula/Aula 13.09/Atividade_7/Program.cs
+++ Atividades de Aula/Aula 13.09/Atividade_7/Program.cs
@@ -6,12 +6,16 @@ namespace Atividade_7
     {
         static void Main(string[] args)
         {
-            int ex7_pass;
-            float ex7_total;
+            // int ex7_pass; 
+            // float ex7_total;
+            // Console.Write("Digite a quantidade de passageiros: ");
+            // ex7_pass = int.Parse(Console.ReadLine());
+            // ex7_total = (ex7_pass*4.3f);
+            // Console.WriteLine("O total de dinheiro arrecadado é: R$ " + ex7_total);
+            
             Console.Write("Digite a quantidade de passageiros: ");
-            ex7_pass = int.Parse(Console.ReadLine());
-            ex7_total = (ex7_pass*4.3f);
-            Console.WriteLine("O total de dinheiro arrecadado é: R$ " + ex7_total);
+            int ex7_pass = int.Parse(Console.ReadLine());
+            Console.WriteLine("O total de dinheiro arrecadado é: R$ " + (ex7_pass * 4.3f));
         }
     }
 }
