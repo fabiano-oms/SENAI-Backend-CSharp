1617a192f5917a7fc201ec13433f0b4d882696cd
FIC <FIC@info.local>
2019-09-16 09:45:15 -0300

    Atividades 6 e 7

---
+++ Atividades de Aula/Aula 13.09/Atividade_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Atividade_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float ex6_sal, ex6_fat, ex6_total;
+            Console.Write("Digite o valor do seu salário: R$ ");
+            ex6_sal = float.Parse(Console.ReadLine());
+            Console.Write("Digite o valor da sua fatura: R$ ");
+            ex6_fat = float.Parse(Console.ReadLine());
+            ex6_total = (ex6_sal-ex6_fat);
+            Console.WriteLine("O valor restante após o pagamento é de: R$ " + ex6_total);
+        }
+    }
+}
---
+++ Atividades de Aula/Aula 13.09/Atividade_7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atividade_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ex7_pass;
+            float ex7_total;
+            Console.Write("Digite a quantidade de passageiros: ");
+            ex7_pass = int.Parse(Console.ReadLine());
+            ex7_total = (ex7_pass*4.3f);
+            Console.WriteLine("O total de dinheiro arrecadado é: R$ " + ex7_total);
+        }
+    }
+}
