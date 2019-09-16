70aee63b24d71c0dc02a026ef3787fa36669c971
FIC <FIC@info.local>
2019-09-16 12:04:09 -0300

    Aula 2 conteudo

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -6,7 +6,25 @@ namespace Aula2
     {
         static void Main(string[] args)
         {
-            
+            // Declaramos as notas que utilizaremos
+            double nota1, nota2, nota3, media;
+
+            // Capturamos todas as notas respectivamente
+            Console.WriteLine("Digite a sua primeira, segunda e terceira nota, respectivamente: ");
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+            nota3 = double.Parse(Console.ReadLine());
+
+            // Calculamos a media
+            media = ((nota1+nota2+nota3)/3);
+
+            // Calculo do condicional
+            if(media >= 6){
+                Console.WriteLine("Aprovado!, média: "+media);
+            } 
+            else{
+                Console.WriteLine($"Reprovado! {media}");
+            }
         }
     }
 }
