55a6f014ccdf4063683d4d911073df09369fa2f1
FIC <FIC@info.local>
2019-09-24 16:43:06 -0300

    Aula 5

---
+++ Atividades de Aula/Aula 23.09/Atividade_1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Atividade_1 {
+    class Program {
+        static void Main (string[] args) {
+            string[] nome = new string[10];
+            float[] preco = new float[10];
+            int op = 9;
+            int p = 0;
+            bool voltar = true;
+
+            const int v = 10;
+            const int w = 9;
+
+            while (op != 0) {
+                if (p < v) {
+                    voltar = true;
+                }
+
+                Console.WriteLine (
+                    @"MENU: 
+                    1 - Cadastrar um produto que terá nome e um preço
+                    2 - Listar todos os produto
+                    3 - Calcular a soma de todos os preços dos produtos
+                    
+                    0 - Sair"
+                );
+                Console.Write ("Digite uma opção: ");
+                op = int.Parse (Console.ReadLine ());
+
+                switch (op) {
+                    case 1:
+                        //Cadastrar um produto que terá nome e um preço
+                        if (p == v) {
+                            Console.WriteLine ("Não é possivel cadastrar mais produtos.");
+                        } else {
+                            while (voltar == true) {
+                                Console.Write ("Insira o nome do " + (p + 1) + "° produto: ");
+                                nome[p] = Console.ReadLine ();
+
+                                Console.Write ("Insira o valor do " + (p + 1) + "° produto: ");
+                                preco[p] = float.Parse (Console.ReadLine ());
+
+                                if (p <= w) {
+                                    Console.WriteLine ("Gostaria de cadastrar outro produto? [true/false]");
+                                    voltar = bool.Parse (Console.ReadLine ());
+                                    p++;
+                                }
+
+                                if (p == v) {
+                                    voltar = false;
+                                    Console.WriteLine ("////////////////////////////");
+                                    Console.WriteLine ("Não é possivel cadastrar mais produtos.");
+                                }
+                                Console.WriteLine ("////////////////////////////");
+                            }
+                        }
+                    break;
+
+                    case 2:
+                        //Listar todos os produto
+                        for (int c = 0; c <= (p - 1); c++) {
+                            Console.WriteLine ((c + 1) + "° produto: " + nome[c]);
+                            Console.WriteLine ((c + 1) + "° preço: " + preco[c]);
+                            Console.WriteLine ("////////////////////////////");
+                        }
+                    break;
+
+                    case 3:
+                        //Calcular a soma de todos os preços dos produtos
+                        float total = 0;
+                        for (int c = 0; c <= p; c++) {
+                            total = total + preco[c];
+                        }
+                        Console.WriteLine ("A soma total de todos os " + p + " produtos é: " + total);
+                        Console.WriteLine ("////////////////////////////");
+                    break;
+
+                    default:
+                        //Nenhuma opção escolhida
+                        Console.WriteLine ("////////////////////////////");
+                        Console.WriteLine ("Obrigado pela preferencia! Volte sempre!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Aula5/Modificado/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Modificado {
+    class Program {
+        static void Main (string[] args) {
+            string[] nome = new string[5];
+            string[] origem = new string[5];
+            string[] destino = new string[5];
+            string[] data_voo = new string[5];
+            int p = 0;
+            bool voltar = true;
+
+            int op = 5;
+
+            while (op != 0) {
+                if (p < 5) {
+                    voltar = true;
+                }
+
+                Console.WriteLine (
+                    "\n 1 - Cadastrar Passagem \n " +
+                    "2 - Listar Passagens \n " +
+                    "0 - Sair \n" +
+                    "------------------\n" +
+                    "------------------\n"
+                );
+                Console.Write ("Digite uma opção: ");
+                op = int.Parse (Console.ReadLine ());
+
+                switch (op) {
+                    case 1:
+                        //Cadastrar Passagem
+                        if (p == 5) {
+                            Console.WriteLine ("Não é possivel cadastrar mais passageiros.");
+                        } else {
+                            while (voltar == true) {
+                                Console.Write ("Digite o nome do " + (p + 1) + "° passageiro: ");
+                                nome[p] = Console.ReadLine ();
+
+                                Console.Write ("Digite a origem do " + (p + 1) + "° passageiro: ");
+                                origem[p] = Console.ReadLine ();
+
+                                Console.Write ("Digite o destino do " + (p + 1) + "° passageiro: ");
+                                destino[p] = Console.ReadLine ();
+
+                                Console.Write ("Digite o data do voo do " + (p + 1) + "° passageiro: ");
+                                data_voo[p] = Console.ReadLine ();
+
+                                if (p <= 4) {
+                                    Console.WriteLine ("Gostaria de cadastrar outro passageiro? [true/false]");
+                                    voltar = bool.Parse (Console.ReadLine ());
+                                    p++;
+                                }
+
+                                if (p == 5) {
+                                    voltar = false;
+                                    Console.WriteLine ("Não é possivel cadastrar mais passageiros.");
+                                }
+
+                                Console.WriteLine ("////////////////////////////");
+                                Console.WriteLine ("////////////////////////////");
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        //Listar Passagens
+                        for (int c = 0; c < p; c++) {
+                            Console.WriteLine ("O nome do " + (c + 1) + "° passageiro: " + nome[c]);
+                            Console.WriteLine ("A origem do " + (c + 1) + "° passageiro: " + origem[c]);
+                            Console.WriteLine ("O destino do " + (c + 1) + "° passageiro: " + destino[c]);
+                            Console.WriteLine ("O data do voo do " + (c + 1) + "° passageiro: " + data_voo[c]);
+                            Console.WriteLine ("////////////////////////////");
+                            Console.WriteLine ("////////////////////////////");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine ("////////////////////////////");
+                        Console.WriteLine ("////////////////////////////");
+                        Console.WriteLine ("Obrigado pela preferencia! Volte sempre!");
+                        break;
+                }
+            }
+        }
+    }
+}
