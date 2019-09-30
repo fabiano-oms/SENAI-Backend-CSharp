62e18d42db6a71493e60b598cb0f813bdcf75a95
FIC <FIC@info.local>
2019-09-30 09:58:54 -0300

    Aula 10

---
+++ Aula10-POO/Senai.POO.CadastroDeAlunos.ComPOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Senai.POO.CadastroDeAlunos.ComPOO.Controllers;
+
+namespace Senai.POO.CadastroDeAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando Objeto
+            AlunoControllers aluno1 = new AlunoControllers(); // Declarando uma nova instância
+
+            // Limpa a tela
+            Console.Clear();
+
+            // Cadastra um aluno.
+            aluno1.CadastrarAluno();
+
+            // Exibindo as informações
+            aluno1.ExibirAluno();
+
+            // Chamando método do objeto 'aluno1'
+            aluno1.IrNoBanheiro();
+        }
+    }
+}
